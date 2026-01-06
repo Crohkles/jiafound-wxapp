8b7d738dfdedbfcdfb374b8eb2d96de80ea2bc13
saikewei <58879129+saikewei@users.noreply.github.com>
2026-01-06 23:35:48 +0800

    加一个拦截器插入url

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,21 @@
 import { createSSRApp } from "vue";
 import App from "./App.vue";
+import { BASE_URL } from './config'
+
+uni.addInterceptor('request', {
+  invoke(args) {
+    // 如果 url 不是以 http 开头，则拼接全局域名
+    if (!args.url.startsWith('http')) {
+      args.url = BASE_URL + args.url
+    }
+    
+    args.header = {
+      ...args.header,
+      'Authorization': 'Bearer ' + (uni.getStorageSync('token') || '')
+    }
+  }
+})
+
 export function createApp() {
   const app = createSSRApp(App);
   return {
